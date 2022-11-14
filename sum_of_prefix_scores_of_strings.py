6c81af9336172b70ffbd7399a3da4c0ce20d9820
owl-legs <46388737+owl-legs@users.noreply.github.com>
2022-11-14 10:12:10 -0800

    Create sum_of_prefix_scores_of_strings.py

---
+++ sum_of_prefix_scores_of_strings.py
@@ -0,0 +1,56 @@
+class Node:
+
+    def __init__(self, label=None):
+
+        self.label = label  
+        self.count = 0
+        self.children = {}
+
+class Trie:
+
+    def __init__(self):
+
+        self.root = Node()
+        self.prefix_sums = {}
+
+    def insert(self, word):
+
+        trie = self.root
+
+        for c in word:
+
+            if c not in trie.children:
+
+                trie.children[c] = Node(c)
+
+            trie.children[c].count += 1
+            trie = trie.children[c]
+
+    def get_word_scores(self, word):
+
+        trie = self.root
+        count = 0
+
+        for c in word:
+
+            count += trie.children[c].count
+            trie = trie.children[c]
+            
+        return(count)
+
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+
+        trie = Trie()
+
+        for word in words:
+
+            trie.insert(word)
+
+        word_scores = []
+
+        for word in words:
+
+            word_scores.append(trie.get_word_scores(word))
+
+        return(word_scores)
